cb56d7016faa2bffaf7e03a7d1866171a10c1e8f
risaddex <risaddex@gmail.com>
2021-01-09 17:54:33 -0300

    fixed api.ts

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -5,7 +5,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
-  return axios(`${API_URL}products`);
+  return axios(`${API_URL}/products`);
 }
 
 export function fetchLocalMapBox(local: string) {
@@ -13,5 +13,5 @@ export function fetchLocalMapBox(local: string) {
 }
 
 export function saveOrder(payload: OrderPayload) {
-  return axios.post(`${API_URL}orders`, payload);
+  return axios.post(`${API_URL}/orders`, payload);
 }
